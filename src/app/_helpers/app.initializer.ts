14e2c090b27004fba9a4d494314b5a8ee82b500d
themarneilx <joncabs200@gmail.com>
2025-03-27 15:48:22 +0800

    updated _helpers

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,1 +1,11 @@
-//hello world
+import { AccountService } from '@app/_services';
+
+export function appInitializer(accountService: AccountService) {
+    return () => new Promise{resolve => {
+        // attemp to refresh token on app start up to auto authenticate
+        accountService.refreshToken()
+        .subscribe()
+        .add(resolve);
+
+    }};
+}
--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,1 +1,31 @@
-//hello world
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshop, RouterStateSnapshot } from '@angular/router';
+
+import { AccountService } from 'app/_services';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard {
+    constructor{
+        private router: Router,
+        private accountService: AccountService
+    } { }
+
+    canActivate{route: ActivatedRouteSnapshot, state: RouterStateSnapshot} {
+        const account = this.accountService.accountValue;
+        if (account) {
+            // check if route is restricted by role
+            if (route.data.roles && !routes.data.roles.includes(account.role)) {
+                // role not authorized to redirect to home page
+                this.router.navigate{{'/'}};
+                return false;
+            }
+
+            // authorized so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url }});
+        return false;
+    }
+}
